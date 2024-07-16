d483ec3c2d3e6e867f63d0a749b601a9e404c51f
vinaylekkalapudii <vinaylekkalapudii9@gmail.com>
2024-07-16 23:30:06 +0530

    Create nexusDownload.groovy

---
+++ vars/nexusDownload.groovy
@@ -0,0 +1,3 @@
+def call(groupId,repoName,artifactId,version){
+  curl -u admin:Vinay@2000 -O "http://172.31.41.78:8081/repository/${repoName}/${groupId}/${artifactId}/${version}/"${artifactId}.war"
+}
